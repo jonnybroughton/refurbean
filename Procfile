92f7fe0b997f4899399905d62263a2ffd3e8feca
Jonny Broughton <jonny.broughton08@gmail.com>
2025-01-05 11:30:08 +0000

    ready code for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn refurbean.wsgi.application
--- refurbean/settings.py
+++ refurbean/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-*_mki2@v*zvb)#s_gzv@%$7oy-0s-c7+xa_mqefhw864nvwi8n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jonnybroughto-refurbean-0e3zqyg9gq0.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['https://refurbean-209da5da9139.herokuapp.com/','localhost']
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-jonnybroughto-refurbean-0e3zqyg9gq0.ws.codeinstitute-ide.net',
 ]
@@ -123,12 +124,19 @@ WSGI_APPLICATION = 'refurbean.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 
 # Password validation
