40448c4fc810142248ac00e7fbf732a328e62929
Jonny Broughton <jonny.broughton08@gmail.com>
2025-01-05 12:16:48 +0000

    remove secret key and set debug

--- refurbean/settings.py
+++ refurbean/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*_mki2@v*zvb)#s_gzv@%$7oy-0s-c7+xa_mqefhw864nvwi8n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['refurbean-209da5da9139.herokuapp.com','localhost']
 CSRF_TRUSTED_ORIGINS = [
