9370f839efc36e561fbdbcefa8ae66e6456b40cc
Jonny Broughton <jonny.broughton08@gmail.com>
2025-01-13 10:39:29 +0000

    fix form error in product_detail

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+import logging
 
 def bag_contents(request):
     bag_items = []
